ef90cb434264cf331f6913b1c8def7959ce75ee4
Yonelexpe <j.a.elexpe@gmail.com>
2022-05-03 13:52:12 +0200

    preguntas

--- src/main/java/Juegi.java
+++ src/main/java/Juegi.java
@@ -27,17 +27,16 @@ public class Juegi {
 	private static final int OPCION_OBSERVAR = 1;
 	private static final int OPCION_PREGUNTAR = 2;
 	private static final int OPCION_RESOLVER = 3;
-	
 
 	public static void main(String[] args) {
-		
+
 		int opciones;
 		boolean repetir = true;
 		verIntro();
-		
+
 		do {
 			opciones = menu();
-	
+
 			switch (opciones) {
 			case OPCION_OBSERVAR:
 				observar();
@@ -53,18 +52,95 @@ public class Juegi {
 				repetir = false;
 				break;
 			}
-			
-		}while(repetir);		
+
+		} while (repetir);
 
 	}// main
 
 	private static void preguntar() {
-		// TODO Auto-generated method stub
+		boolean flag1 = true;
+		boolean flag2 = true;
+		boolean flag3 = true;
+		int ridpe =0;
+		int ridpre =0;
 		
-		
-		
-		
-	}
+		try (Connection con = Conexion.getConnection();
+				// pregunta a quien
+				PreparedStatement pst1 = con.prepareStatement("SELECT id_persona,nombre FROM personas;");
+				 //pregunta que quieres preguntar
+				PreparedStatement pst2 = con.prepareStatement("SELECT id_pregunta, texto FROM preguntas;");
+				//respuesta
+				PreparedStatement pst3 = con.prepareStatement(
+						"SELECT respuesta, nombre FROM personas p, persona_pregunta pp, preguntas \r\n"
+								+ "WHERE p.id_persona=pp.id_persona AND pp.id_pregunta = preguntas.id_pregunta\r\n"
+								+ "AND p.id_persona =  ? AND preguntas.id_pregunta= ?  ;");//TODO cambiar nº por ?
+				ResultSet rs1 = pst1.executeQuery();) {
+
+			System.out.println("A quien quieres preguntar");
+			while (rs1.next()) {
+				String nombre = rs1.getString("nombre");
+				int idnombre = rs1.getInt("id_persona");
+				System.out.printf("%-2s : %s \n", idnombre, nombre);
+			}
+			// elegir id_persona
+			while (flag1) {
+				 ridpe = Integer.parseInt(sc.nextLine());
+
+				if (ridpe < 1 || ridpe > 3) {
+					System.out.println("error introduce un numero correcto");
+				} else {
+					flag1 = false;
+				} // fin else
+			} // finwhile
+			
+			
+			System.out.println("que quieres preguntarle");
+			ResultSet rs2 = pst2.executeQuery();
+			
+			while (rs2.next()) {
+				String texto = rs2.getString("texto");
+				int idpregun = rs2.getInt("id_pregunta");
+				System.out.printf("%-2s : %s \n", idpregun, texto);
+			}
+			
+			//elegir pregunta
+			
+			
+			while (flag2) {
+				 ridpre = Integer.parseInt(sc.nextLine());
+
+				if (ridpre < 1 || ridpre > 1) {
+					System.out.println("error introduce un numero correcto");
+				} else {
+					flag2 = false;
+				} // fin else
+			} // finwhile
+			
+			
+			// mostrar resultados
+			
+	pst3.setInt(1, ridpe);
+	pst3.setInt(2, ridpre);
+	
+			ResultSet rs3= pst3.executeQuery();
+			
+			while (rs3.next()) {
+				
+				String respuesta = rs3.getString("respuesta");
+				String perso = rs3.getString("nombre");
+					System.out.printf("%s te contesta: \n %s\n\n",perso,respuesta);
+					
+					
+				
+			}
+			
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} // catch
+
+	}// try
 
 	private static void verIntro() {
 		try (Connection con = Conexion.getConnection(); // mostar introduccion
@@ -84,7 +160,7 @@ public class Juegi {
 			// TODO: handle exception
 			System.out.println("error algo");
 		} // CATCH
-		
+
 	}
 
 	private static void observar() {
@@ -139,39 +215,34 @@ public class Juegi {
 
 				System.out.printf("%-2s : %s \n", id, obje);
 
-			
-
 			} // rs.next
 
-			if (rhubi==1) {
+			if (rhubi == 1) {
 				while (flag2) {
 					robje = Integer.parseInt(sc.nextLine());
-					
-					if (robje <1 || robje >2) {
+
+					if (robje < 1 || robje > 2) {
 						System.out.println("error introduce un numero correcto");
 					} else {
 						flag2 = false;
 					}
-					
-				}//fin while
-				
-				
-				
-				
+
+				} // fin while
+
 			} else {
 				while (flag2) {
 					robje = Integer.parseInt(sc.nextLine());
-					
-					if (robje <3 || robje >4) {
+
+					if (robje < 3 || robje > 4) {
 						System.out.println("error introduce un numero correcto");
 					} else {
 						flag2 = false;
 					}
+				}
 			}
-			}
-			
+
 			// descricion del objeto
-			
+
 			pst3.setInt(1, rhubi);
 			pst3.setInt(2, robje);
 
@@ -180,13 +251,12 @@ public class Juegi {
 			while (rs3.next()) {
 				String descri = rs3.getString("descripcion");
 
-				System.out.printf("%s \n", descri);
+				System.out.printf("%s \n\n", descri);
 
 				// TODO hacer exsecciones
 
 			} // rs.next
 
-			
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
